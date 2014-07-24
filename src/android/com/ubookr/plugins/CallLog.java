b3b4155ee4a3f4ab214feddddf575709d6f0aecc
Colin Daly <mr.colin.daly@gmail.com>
2014-07-24 16:15:42 +1000

    Trying to sort out JSON Exception handling.

--- src/android/com/ubookr/plugins/CallLog.java
+++ src/android/com/ubookr/plugins/CallLog.java
@@ -24,7 +24,7 @@ import java.util.Date;
 
 public class CallLog extends CordovaPlugin {
 
-    private static final String ACTION_ALL = "all";
+//    private static final String ACTION_ALL = "all";
     private static final String ACTION_LIST = "list";
     private static final String ACTION_CONTACT = "contact";
     private static final String ACTION_SHOW = "show";
@@ -37,29 +37,15 @@ public class CallLog extends CordovaPlugin {
         Log.d(TAG, "Plugin Called");
         PluginResult result = null;
 
-        if (ACTION_ALL.equals(action)) {
-            JSONObject all = getAllCallLog(args);
-            result = new PluginResult(PluginResult.Status.OK, all);
-        } else if (ACTION_CONTACT.equals(action)) {
-            String phoneNumber = args.getString(0);
-            result = contact(phoneNumber);
+//        if (ACTION_ALL.equals(action)) {
+//            JSONObject all = getAllCallLog(args);
+//            result = new PluginResult(PluginResult.Status.OK, all);
+        if (ACTION_CONTACT.equals(action)) {
+            result = contact(args);
         } else if (ACTION_SHOW.equals(action)) {
-            String phoneNumber = args.getString(0);
-            result = show(phoneNumber);
+            result = show(args);
         } else if (ACTION_LIST.equals(action)) {
-            int days = 1;
-            //obtain date to limit by
-            if ( ! args.isNull(0)) {
-                String period = args.getString(0);
-                Log.d(TAG, "Time period is: " + period);
-                if (period.equals("week"))
-                    days = 7;
-                else if (period.equals("month"))
-                    days = 30;
-                else if (period.equals("all"))
-                    days = -1; // indicates no limit in list method below
-            }
-            result = list(days);
+            result = list(args);
         } else {
             Log.d(TAG, "Invalid action : " + action + " passed");
             result = new PluginResult(Status.INVALID_ACTION);
@@ -68,12 +54,16 @@ public class CallLog extends CordovaPlugin {
         return true;
     }
 
-    private PluginResult show(String phoneNumber) {
+    private PluginResult show(JSONArray args) {
 
         PluginResult result;
         try {
+            String phoneNumber = args.getString(0);
             viewContact(phoneNumber);
             result = new PluginResult(Status.OK);
+        } catch (JSONException e) {
+            Log.d(TAG, "Got JSON Exception " + e.getMessage());
+            result = new PluginResult(Status.JSON_EXCEPTION, e.getMessage());
         } catch (Exception e) {
             Log.d(TAG, "Got Exception " + e.getMessage());
             result = new PluginResult(Status.ERROR, e.getMessage());
@@ -85,20 +75,38 @@ public class CallLog extends CordovaPlugin {
 
         PluginResult result;
         try {
+            String phoneNumber = args.getString(0);
             String contactInfo = getContactNameFromNumber(phoneNumber);
             Log.d(TAG, "Returning " + contactInfo.toString());
             result = new PluginResult(Status.OK, contactInfo);
+        } catch (JSONException e) {
+            Log.d(TAG, "Got JSON Exception " + e.getMessage());
+            result = new PluginResult(Status.JSON_EXCEPTION, e.getMessage());
         } catch (Exception e) {
             Log.d(TAG, "Got Exception " + e.getMessage());
-            result = new PluginResult(Status.JSON_EXCEPTION, e.getMessage());
+            result = new PluginResult(Status.ERROR, e.getMessage());
         }
         return result;
     }
 
-    private PluginResult list(int days) {
+    private PluginResult list(JSONArray args) {
 
         PluginResult result;
         try {
+
+            int days = 1; // default to a day
+            //obtain date to limit by
+            if ( ! args.isNull(0)) {
+                String period = args.getString(0);
+                Log.d(TAG, "Time period is: " + period);
+                if (period.equals("week"))
+                    days = 7;
+                else if (period.equals("month"))
+                    days = 30;
+                else if (period.equals("all"))
+                    days = -1;
+            }
+
             String limiter = null;
             if (days > 0) {
                 //turn this into a date
@@ -118,6 +126,9 @@ public class CallLog extends CordovaPlugin {
         } catch (JSONException e) {
             Log.d(TAG, "Got JSON Exception " + e.getMessage());
             result = new PluginResult(Status.JSON_EXCEPTION, e.getMessage());
+        } catch (Exception e) {
+            Log.d(TAG, "Got Exception " + e.getMessage());
+            result = new PluginResult(Status.ERROR, e.getMessage());
         }
         return result;
     }
@@ -181,85 +192,6 @@ public class CallLog extends CordovaPlugin {
    		return callLog;
    	}
 
-//        JSONObject callLogs = new JSONObject();
-//
-//        try {
-//            callLogs = getAllCallLog(args);
-//            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, callLogs));
-//            return true;
-////            switch (getActionItem(actionName)) {
-////                case 1:
-////                    callLogs = getAllCallLog(arguments);
-////                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, callLogs));
-////                    return true;
-////                default: {
-////                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
-////                }
-////            }
-//        } catch (JSONException e) {
-//            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.JSON_EXCEPTION, e.getMessage()));
-//        }
-//
-//        return false;
-//    }
-
-    private JSONObject getAllCallLog(JSONArray requirements) throws JSONException {
-        JSONObject callLog = new JSONObject();
-
-        String[] strFields = {
-                android.provider.CallLog.Calls.DATE,
-                android.provider.CallLog.Calls.NUMBER,
-                android.provider.CallLog.Calls.TYPE,
-                android.provider.CallLog.Calls.DURATION,
-                android.provider.CallLog.Calls.NEW,
-                android.provider.CallLog.Calls.CACHED_NAME,
-                android.provider.CallLog.Calls.CACHED_NUMBER_TYPE,
-                android.provider.CallLog.Calls.CACHED_NUMBER_LABEL//,
-        };
-
-        try {
-            Cursor callLogCursor = this.cordova.getActivity().getContentResolver().query(
-                    android.provider.CallLog.Calls.CONTENT_URI,
-                    strFields,
-                    null,
-                    null,
-                    android.provider.CallLog.Calls.DEFAULT_SORT_ORDER
-            );
-
-
-            int callCount = callLogCursor.getCount();
-
-            if (callCount > 0) {
-                JSONArray callLogItem = new JSONArray();
-                JSONArray callLogItems = new JSONArray();
-
-                String[] columnNames = callLogCursor.getColumnNames();
-
-                callLogCursor.moveToFirst();
-                do {
-                    callLogItem.put(callLogCursor.getLong(0));
-                    callLogItem.put(callLogCursor.getString(1));
-                    callLogItem.put(callLogCursor.getInt(2));
-                    callLogItem.put(callLogCursor.getLong(3));
-                    callLogItem.put(callLogCursor.getInt(4));
-                    callLogItem.put(callLogCursor.getString(5));
-                    callLogItem.put(callLogCursor.getInt(6));
-                    callLogItems.put(callLogItem);
-                    callLogItem = new JSONArray();
-
-                } while (callLogCursor.moveToNext());
-
-                callLog.put("Rows", callLogItems);
-            }
-
-            callLogCursor.close();
-        } catch (Exception e) {
-            Log.d("CallLog_Plugin", " ERROR : SQL to get cursor: ERROR " + e.getMessage());
-        }
-
-        return callLog;
-    }
-
     private String getContactNameFromNumber(String number) {
 
         // define the columns I want the query to return
@@ -282,6 +214,85 @@ public class CallLog extends CordovaPlugin {
         // return the original number if no match was found
         return number;
     }
+
+//        JSONObject callLogs = new JSONObject();
+//
+//        try {
+//            callLogs = getAllCallLog(args);
+//            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, callLogs));
+//            return true;
+////            switch (getActionItem(actionName)) {
+////                case 1:
+////                    callLogs = getAllCallLog(arguments);
+////                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, callLogs));
+////                    return true;
+////                default: {
+////                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
+////                }
+////            }
+//        } catch (JSONException e) {
+//            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.JSON_EXCEPTION, e.getMessage()));
+//        }
+//
+//        return false;
+//    }
+
+//    private JSONObject getAllCallLog(JSONArray requirements) throws JSONException {
+//        JSONObject callLog = new JSONObject();
+//
+//        String[] strFields = {
+//                android.provider.CallLog.Calls.DATE,
+//                android.provider.CallLog.Calls.NUMBER,
+//                android.provider.CallLog.Calls.TYPE,
+//                android.provider.CallLog.Calls.DURATION,
+//                android.provider.CallLog.Calls.NEW,
+//                android.provider.CallLog.Calls.CACHED_NAME,
+//                android.provider.CallLog.Calls.CACHED_NUMBER_TYPE,
+//                android.provider.CallLog.Calls.CACHED_NUMBER_LABEL//,
+//        };
+//
+//        try {
+//            Cursor callLogCursor = this.cordova.getActivity().getContentResolver().query(
+//                    android.provider.CallLog.Calls.CONTENT_URI,
+//                    strFields,
+//                    null,
+//                    null,
+//                    android.provider.CallLog.Calls.DEFAULT_SORT_ORDER
+//            );
+//
+//
+//            int callCount = callLogCursor.getCount();
+//
+//            if (callCount > 0) {
+//                JSONArray callLogItem = new JSONArray();
+//                JSONArray callLogItems = new JSONArray();
+//
+//                String[] columnNames = callLogCursor.getColumnNames();
+//
+//                callLogCursor.moveToFirst();
+//                do {
+//                    callLogItem.put(callLogCursor.getLong(0));
+//                    callLogItem.put(callLogCursor.getString(1));
+//                    callLogItem.put(callLogCursor.getInt(2));
+//                    callLogItem.put(callLogCursor.getLong(3));
+//                    callLogItem.put(callLogCursor.getInt(4));
+//                    callLogItem.put(callLogCursor.getString(5));
+//                    callLogItem.put(callLogCursor.getInt(6));
+//                    callLogItems.put(callLogItem);
+//                    callLogItem = new JSONArray();
+//
+//                } while (callLogCursor.moveToNext());
+//
+//                callLog.put("Rows", callLogItems);
+//            }
+//
+//            callLogCursor.close();
+//        } catch (Exception e) {
+//            Log.d("CallLog_Plugin", " ERROR : SQL to get cursor: ERROR " + e.getMessage());
+//        }
+//
+//        return callLog;
+//    }
 //
 ////    private JSONObject getTimeRangeCallLog(JSONArray requirements) {
 ////    }
