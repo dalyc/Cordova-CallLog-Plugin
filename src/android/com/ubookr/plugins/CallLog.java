3e1d683f9478627bd9100c4cc7631aa2e879a2a9
Colin Daly <mr.colin.daly@gmail.com>
2014-07-24 11:30:46 +1000

    Rewrite CallLog.js and use echo method only to test deploy.

--- src/android/com/ubookr/plugins/CallLog.java
+++ src/android/com/ubookr/plugins/CallLog.java
@@ -12,95 +12,112 @@ import org.apache.cordova.PluginResult;
 public class CallLog extends CordovaPlugin {
 
     @Override
-    public boolean execute(String actionName, JSONArray arguments, final CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 
-        JSONObject callLogs = new JSONObject();
-
-        try {
-            callLogs = getAllCallLog(arguments);
-            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, callLogs));
+        if (action.equals("echo")) {
+            String message = args.getString(0);
+            this.echo(message, callbackContext);
             return true;
-//            switch (getActionItem(actionName)) {
-//                case 1:
-//                    callLogs = getAllCallLog(arguments);
-//                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, callLogs));
-//                    return true;
-//                default: {
-//                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
-//                }
-//            }
-        } catch (JSONException jsonEx) {
-            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.JSON_EXCEPTION));
         }
 
         return false;
     }
 
-    private JSONObject getAllCallLog(JSONArray requirements) throws JSONException {
-        JSONObject callLog = new JSONObject();
+    private void echo(String message, CallbackContext callbackContext) {
 
-        String[] strFields = {
-                android.provider.CallLog.Calls.DATE,
-                android.provider.CallLog.Calls.NUMBER,
-                android.provider.CallLog.Calls.TYPE,
-                android.provider.CallLog.Calls.DURATION,
-                android.provider.CallLog.Calls.NEW,
-                android.provider.CallLog.Calls.CACHED_NAME,
-                android.provider.CallLog.Calls.CACHED_NUMBER_TYPE,
-                android.provider.CallLog.Calls.CACHED_NUMBER_LABEL//,
-        };
-
-        try {
-            Cursor callLogCursor = this.cordova.getActivity().getContentResolver().query(
-                    android.provider.CallLog.Calls.CONTENT_URI,
-                    strFields,
-                    null,
-                    null,
-                    android.provider.CallLog.Calls.DEFAULT_SORT_ORDER
-            );
-
-
-            int callCount = callLogCursor.getCount();
-
-            if (callCount > 0) {
-                JSONArray callLogItem = new JSONArray();
-                JSONArray callLogItems = new JSONArray();
-
-                String[] columnNames = callLogCursor.getColumnNames();
-
-                callLogCursor.moveToFirst();
-                do {
-                    callLogItem.put(callLogCursor.getLong(0));
-                    callLogItem.put(callLogCursor.getString(1));
-                    callLogItem.put(callLogCursor.getInt(2));
-                    callLogItem.put(callLogCursor.getLong(3));
-                    callLogItem.put(callLogCursor.getInt(4));
-                    callLogItem.put(callLogCursor.getString(5));
-                    callLogItem.put(callLogCursor.getInt(6));
-                    callLogItems.put(callLogItem);
-                    callLogItem = new JSONArray();
-
-                } while (callLogCursor.moveToNext());
-
-                callLog.put("Rows", callLogItems);
-            }
-
-            callLogCursor.close();
-        } catch (Exception e) {
-            Log.d("CallLog_Plugin", " ERROR : SQL to get cursor: ERROR " + e.getMessage());
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
         }
-
-        return callLog;
+    }
+//        JSONObject callLogs = new JSONObject();
+//
+//        try {
+//            callLogs = getAllCallLog(args);
+//            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, callLogs));
+//            return true;
+////            switch (getActionItem(actionName)) {
+////                case 1:
+////                    callLogs = getAllCallLog(arguments);
+////                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, callLogs));
+////                    return true;
+////                default: {
+////                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
+////                }
+////            }
+//        } catch (JSONException jsonEx) {
+//            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.JSON_EXCEPTION));
+//        }
+//
+//        return false;
     }
 
-//    private JSONObject getTimeRangeCallLog(JSONArray requirements) {
+//    private JSONObject getAllCallLog(JSONArray requirements) throws JSONException {
+//        JSONObject callLog = new JSONObject();
+//
+//        String[] strFields = {
+//                android.provider.CallLog.Calls.DATE,
+//                android.provider.CallLog.Calls.NUMBER,
+//                android.provider.CallLog.Calls.TYPE,
+//                android.provider.CallLog.Calls.DURATION,
+//                android.provider.CallLog.Calls.NEW,
+//                android.provider.CallLog.Calls.CACHED_NAME,
+//                android.provider.CallLog.Calls.CACHED_NUMBER_TYPE,
+//                android.provider.CallLog.Calls.CACHED_NUMBER_LABEL//,
+//        };
+//
+//        try {
+//            Cursor callLogCursor = this.cordova.getActivity().getContentResolver().query(
+//                    android.provider.CallLog.Calls.CONTENT_URI,
+//                    strFields,
+//                    null,
+//                    null,
+//                    android.provider.CallLog.Calls.DEFAULT_SORT_ORDER
+//            );
+//
+//
+//            int callCount = callLogCursor.getCount();
+//
+//            if (callCount > 0) {
+//                JSONArray callLogItem = new JSONArray();
+//                JSONArray callLogItems = new JSONArray();
+//
+//                String[] columnNames = callLogCursor.getColumnNames();
+//
+//                callLogCursor.moveToFirst();
+//                do {
+//                    callLogItem.put(callLogCursor.getLong(0));
+//                    callLogItem.put(callLogCursor.getString(1));
+//                    callLogItem.put(callLogCursor.getInt(2));
+//                    callLogItem.put(callLogCursor.getLong(3));
+//                    callLogItem.put(callLogCursor.getInt(4));
+//                    callLogItem.put(callLogCursor.getString(5));
+//                    callLogItem.put(callLogCursor.getInt(6));
+//                    callLogItems.put(callLogItem);
+//                    callLogItem = new JSONArray();
+//
+//                } while (callLogCursor.moveToNext());
+//
+//                callLog.put("Rows", callLogItems);
+//            }
+//
+//            callLogCursor.close();
+//        } catch (Exception e) {
+//            Log.d("CallLog_Plugin", " ERROR : SQL to get cursor: ERROR " + e.getMessage());
+//        }
+//
+//        return callLog;
 //    }
-
-    private int getActionItem (String actionName)throws JSONException {
-        JSONObject actions = new JSONObject("{'all':1,'last':2,'time':3}");
-        if (actions.has(actionName)) {
-            return actions.getInt(actionName);
-        }
-        return 0;
-    }
+//
+////    private JSONObject getTimeRangeCallLog(JSONArray requirements) {
+////    }
+//
+//    private int getActionItem (String actionName)throws JSONException {
+//        JSONObject actions = new JSONObject("{'all':1,'last':2,'time':3}");
+//        if (actions.has(actionName)) {
+//            return actions.getInt(actionName);
+//        }
+//        return 0;
+//    }
 }
--- www/CallLog.js
+++ www/CallLog.js
@@ -1,41 +1,17 @@
-function Toast() {
+function CallLog() {
 }
 
-Toast.prototype.show = function (message, duration, position, successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "Toast", "show", [message, duration, position]);
+CallLog.prototype.show = function (message, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "CallLog", "echo", [message]);
 };
 
-Toast.prototype.showShortTop = function (message, successCallback, errorCallback) {
-  this.show(message, "short", "top", successCallback, errorCallback);
-};
-
-Toast.prototype.showShortCenter = function (message, successCallback, errorCallback) {
-  this.show(message, "short", "center", successCallback, errorCallback);
-};
-
-Toast.prototype.showShortBottom = function (message, successCallback, errorCallback) {
-  this.show(message, "short", "bottom", successCallback, errorCallback);
-};
-
-Toast.prototype.showLongTop = function (message, successCallback, errorCallback) {
-  this.show(message, "long", "top", successCallback, errorCallback);
-};
-
-Toast.prototype.showLongCenter = function (message, successCallback, errorCallback) {
-  this.show(message, "long", "center", successCallback, errorCallback);
-};
-
-Toast.prototype.showLongBottom = function (message, successCallback, errorCallback) {
-  this.show(message, "long", "bottom", successCallback, errorCallback);
-};
-
-Toast.install = function () {
+CallLog.install = function () {
   if (!window.plugins) {
     window.plugins = {};
   }
 
-  window.plugins.toast = new Toast();
-  return window.plugins.toast;
+  window.plugins.calllog = new CallLog();
+  return window.plugins.calllog;
 };
 
-cordova.addConstructor(Toast.install);
+cordova.addConstructor(CallLog.install);
